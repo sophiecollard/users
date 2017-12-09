7b611e485e5c422d02b206c91738c225eb811a2b
Sophie <sophie.collard@hotmail.fr>
2017-12-09 18:37:03 +0000

    Define AdminApi with unimplemented routes

---
+++ src/main/scala/users/api/usermanagement/AdminApi.scala
@@ -0,0 +1,41 @@
+package users.api.usermanagement
+
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+
+import users.domain.User
+
+class AdminApi {
+
+  val userIdMatcher = Segment.map(User.Id(_))
+
+  val routes: Route = pathPrefix("users") {
+    pathEndOrSingleSlash {
+      get {
+        complete(StatusCodes.NotImplemented)
+      }
+    } ~ pathPrefix(userIdMatcher) { id =>
+      pathEndOrSingleSlash {
+        get {
+          complete(StatusCodes.NotImplemented)
+        } ~ delete {
+          complete(StatusCodes.NotImplemented)
+        }
+      } ~ path("reset-password") {
+        post {
+          complete(StatusCodes.NotImplemented)
+        }
+      } ~ path("block") {
+        post {
+          complete(StatusCodes.NotImplemented)
+        }
+      } ~ path("unblock") {
+        post {
+          complete(StatusCodes.NotImplemented)
+        }
+      }
+    }
+  }
+
+}
