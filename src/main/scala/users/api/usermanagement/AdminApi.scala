7c50a667c277fd380f69d0f5cd5cceee7aea0852
Sophie <sophie.collard@hotmail.fr>
2017-12-11 00:33:03 +0000

    Implement and test AdminApi routes

--- src/main/scala/users/api/usermanagement/AdminApi.scala
+++ src/main/scala/users/api/usermanagement/AdminApi.scala
@@ -3,9 +3,10 @@ package users.api.usermanagement
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import io.fcomb.akka.http.CirceSupport._
 
-import users.domain.User
-import users.services._
+import users.domain._
+import users.services.UserManagement
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -16,26 +17,50 @@ class AdminApi(service: UserManagement[Future[?]])(implicit ec: ExecutionContext
   val routes: Route = pathPrefix("users") {
     pathEndOrSingleSlash {
       get {
-        complete(StatusCodes.NotImplemented)
+        val usersF = service.all()
+        onSuccess(usersF) {
+          case Left(error) => complete(error)
+          case Right(users) => complete(users)
+        }
       }
     } ~ pathPrefix(userIdMatcher) { id =>
       pathEndOrSingleSlash {
         get {
-          complete(StatusCodes.NotImplemented)
+          val userF = service.get(id)
+          onSuccess(userF) {
+            case Left(error) => complete(error)
+            case Right(user) => complete(user)
+          }
         } ~ delete {
-          complete(StatusCodes.NotImplemented)
+          val deletedF = service.delete(id)
+          onSuccess(deletedF) {
+            case Left(error) => complete(error)
+            case Right(_) => complete(StatusCodes.OK)
+          }
         }
       } ~ path("reset-password") {
         post {
-          complete(StatusCodes.NotImplemented)
+          val userF = service.resetPassword(id)
+          onSuccess(userF) {
+            case Left(error) => complete(error)
+            case Right(user) => complete(user)
+          }
         }
       } ~ path("block") {
         post {
-          complete(StatusCodes.NotImplemented)
+          val userF = service.block(id)
+          onSuccess(userF) {
+            case Left(error) => complete(error)
+            case Right(user) => complete(user)
+          }
         }
       } ~ path("unblock") {
         post {
-          complete(StatusCodes.NotImplemented)
+          val userF = service.unblock(id)
+          onSuccess(userF) {
+            case Left(error) => complete(error)
+            case Right(user) => complete(user)
+          }
         }
       }
     }
--- src/test/scala/users/api/usermanagement/AdminApiSpec.scala
+++ src/test/scala/users/api/usermanagement/AdminApiSpec.scala
@@ -3,13 +3,20 @@ package users.api.usermanagement
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.Specs2RouteTest
 import com.typesafe.config.ConfigFactory
+import io.fcomb.akka.http.CirceSupport._
 import org.specs2.mutable.Specification
 import pureconfig.syntax._
 
 import users.config.ApplicationConfig
+import users.domain._
 import users.main.Apis
+import users.utils._
 
-class AdminApiSpec extends Specification with Specs2RouteTest {
+class AdminApiSpec extends Specification
+  with Specs2RouteTest
+  with AwaitHelper {
+
+  sequential
 
   val config = ConfigFactory.load().to[ApplicationConfig].get // not safe!
 
@@ -17,44 +24,65 @@ class AdminApiSpec extends Specification with Specs2RouteTest {
 
   val api = apis.adminUserManagement
 
-  "AdminApi" should {
+  val user: User =
+    await(
+      apis
+        .services
+        .userManagement
+        .signUp(
+          userName = UserName("test_user"),
+          emailAddress = EmailAddress("someone@paidy.com"),
+          password = Some(Password("secret_password"))
+        )
+    ).toOption.get
 
+  "AdminApi" should {
     "get all users" in {
       Get("/users") ~> api.routes ~> check {
-        status === StatusCodes.NotImplemented
+        status === StatusCodes.OK
+        responseAs[List[User]] must contain(user)
       }
     }
 
     "get user" in {
-      Get("/users/somerandomuser") ~> api.routes ~> check {
-        status === StatusCodes.NotImplemented
-      }
-    }
-
-    "delete user" in {
-      Delete("/users/somerandomuser") ~> api.routes ~> check {
-        status === StatusCodes.NotImplemented
-      }
-    }
-
-    "reset user password" in {
-      Post("/users/randomuser/reset-password") ~> api.routes ~> check {
-        status === StatusCodes.NotImplemented
+      Get(s"/users/${user.id.value}") ~> api.routes ~> check {
+        status === StatusCodes.OK
+        responseAs[User] === user
       }
     }
 
     "block user" in {
-      Post("/users/randomuser/block") ~> api.routes ~> check {
-        status === StatusCodes.NotImplemented
+      Post(s"/users/${user.id.value}/block") ~> api.routes ~> check {
+        status === StatusCodes.OK
+        val blockedUser = responseAs[User]
+        blockedUser.status === User.Status.Blocked
       }
     }
 
     "unblock user" in {
-      Post("/users/randomuser/unblock") ~> api.routes ~> check {
-        status === StatusCodes.NotImplemented
+      Post(s"/users/${user.id.value}/unblock") ~> api.routes ~> check {
+        status === StatusCodes.OK
+        val activeUser = responseAs[User]
+        activeUser.status === User.Status.Active
       }
     }
 
+    "reset user password" in {
+      Post(s"/users/${user.id.value}/reset-password") ~> api.routes ~> check {
+        status === StatusCodes.OK
+        val updatedUser = responseAs[User]
+        updatedUser.password must beNone
+      }
+    }
+
+    "delete user" in {
+      // users must be blocked before they can be deleted
+      await(apis.services.userManagement.block(user.id)) must beRight
+      Delete(s"/users/${user.id.value}") ~> api.routes ~> check {
+        status === StatusCodes.OK
+      }
+      await(apis.services.userManagement.get(user.id)).toOption.get.status === User.Status.Deleted
+    }
   }
 
 }
